91f42ab426e6ffed07ceb53c4e792d9e917c8ebf
Pasindu Weerasekara <77112190+pasinduweerasekara@users.noreply.github.com>
2024-05-02 00:38:06 +0530

    Add files via upload

---
+++ type_animation/typing_animation/index.js
@@ -0,0 +1,61 @@
+class TypingObject {
+    curPhraseIndex = 0 
+
+    constructor(element, phrases,loop = true, typingSpeed = 100, pause = 1000, deletingSpeed = 50) {
+        this.element = element // Assign the DOM element to be typed into
+        this.phrases = phrases // Assign the array of phrases to be typed
+        this.loop = loop
+        this.curPhraseIndex = 0
+        this.typingSpeed = typingSpeed
+        this.pause = pause
+        this.deletingSpeed = deletingSpeed
+    }
+
+    sleep(milliSeconds) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, milliSeconds) // A helper function to pause execution for a specified time
+        })
+    }
+
+    typeAndDelete = async () => { // An async function to type and delete text
+        let repeat = true
+        while (repeat){ // Loop indefinitely
+            let curPhrase = this.phrases[this.curPhraseIndex]
+            for (let i = 0; i < curPhrase.length; i++) { // Loop through each character in the phrase
+                this.element.innerText = curPhrase.substring(0, i + 1) // Update element with typed characters
+                await this.sleep(this.typingSpeed) // Pause for typing speed duration
+            }
+            await this.sleep(this.pause) // Pause for pause duration after typing
+
+            for (let i = curPhrase.length; i > 0; i--) { // Loop to delete characters
+                this.element.innerText = curPhrase.substring(0, i - 1) 
+                if (i === 1) { // If all characters are deleted
+                    this.element.innerText = "\u00A0"
+                }
+                await this.sleep(this.deletingSpeed)
+            }
+
+            if (this.curPhraseIndex == this.phrases.length - 1) { 
+                if(!this.loop){ //if loop property is false braking the loop
+                    break
+                }
+                this.curPhraseIndex = 0 
+            } else {
+                this.curPhraseIndex++
+            }
+
+        }
+    }
+}
+
+// Create instances of TypingObject with different configurations
+const typingElement1 = new TypingObject(element = document.getElementById("element1"), phrases = ["1 This is a Single time loop", "2 Second phrase of texts","3 Last part of the phrases and fished"],loop=false)
+const typingElement2 = new TypingObject(element = document.getElementById("element2"), phrases = ["1 This is a Normal typing", "2 Infinite", "3 loop", " 4 Again to first phrase"], typingSpeed = 20, pause = 1000)
+const typingElement3 = new TypingObject(element = document.getElementById("element3"), phrases = ["1 Fast deleting Loop","2 Second phrase"], deletingSpeed = 10)
+const typingElement4 = new TypingObject(element = document.getElementById("element4"), phrases = ["1 Slow typing loop", "Like a", "Snail..:;"],loop=true, typingSpeed = 1000, pause = 150, deletingSpeed = 100)
+
+// Start the typing and deleting animation for each element
+typingElement1.typeAndDelete()
+typingElement2.typeAndDelete()
+typingElement3.typeAndDelete()
+typingElement4.typeAndDelete()
